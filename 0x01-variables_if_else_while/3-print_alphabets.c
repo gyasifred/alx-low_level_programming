2c5263435eb9ff720f9b870ddecbe09a0e52d856
gyasifred <gyasifred@gmail.com>
2023-03-18 01:32:17 +0000

    lowcase folllowed by uppercse alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i, j;
+
+	i = 'a';
+	j = 'A';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 'Z')
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+}
