8b4036058ef405ee9802cecb4f1862183440fbe6
gyasifred <gyasifred@gmail.com>
2023-03-21 15:27:54 +0000

    get absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value
+ * @n: nthe absolute number
+ * Return: return the absolute value i.e n or 0
+ */
+int _abs(int n)
+{
+	if (n < 48)
+	{
+		return (n * -1);
+	}
+	else if (n == 48)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
