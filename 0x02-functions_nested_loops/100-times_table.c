922a50d9283e959ccd2c1634ae46f97e9aab03b5
gyasifred <gyasifred@gmail.com>
2023-03-21 23:08:11 +0000

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  *print_times_table - print n time table
+  *@n: integer
+  *Return; void
+  */
+void print_times_table(int n)
+{
+	int row, col, prod;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			prod = row * col;
+
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod < 10)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(32);
+				_putchar(prod % 10 + 48);
+			}
+			else if (prod >= 10 && prod < 100)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar((prod / 10) % 10 + 48);
+				_putchar(prod % 10 + 48);
+			}
+			else
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar((prod / 100) % 10 + 48);
+				_putchar((prod / 10) %  10 + 48);
+				_putchar(prod % 10 + 48);
+			}
+		}
+		_putchar(10);
+	}
+}
