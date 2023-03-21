ec5ece3cab92d25c79dbb5f05b7c4b028c5e36ec
gyasifred <gyasifred@gmail.com>
2023-03-21 15:02:31 +0000

    check sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *print_sign - function to check sign of a number
+  *@n: n is an ASCII character
+  *Return: int
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
