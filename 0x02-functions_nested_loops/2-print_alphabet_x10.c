f2718a3b5521a6fd22cb1702cb9be7231a6a2d88
gyasifred <gyasifred@gmail.com>
2023-03-21 13:43:13 +0000

    print 10x alphabet to newline

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10x the alphabets in lowercase
+ * Return: return 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = 1;
+	while (i <= 10)
+	{
+		j = 97;
+		while (j <= 122)
+		{
+			_putchar(j)
+				j++;
+		}
+		i++;
+		_putchar(10);
+	}
+}
