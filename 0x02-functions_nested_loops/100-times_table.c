9f74c86902f6605b846484c84943b5761f6d3734
gyasifred <gyasifred@gmail.com>
2023-03-22 01:14:19 +0000

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,44 +9,42 @@ void print_times_table(int n)
 {
 	int row, col, prod;
 
-    if (n >= 0 && n < 15)
-    {
-	    for (row = 0; row <= n; row++)
-	    {
-		    for (col = 0; col <= n; col++)
-		    {
-			    prod = row * col;
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				prod = row * col;
 
-			    if (col == 0)
-			    {
-				    _putchar('0');
-			    }
-			    else if (prod < 10)
-			    {
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				_putchar(32);
-				_putchar(prod % 10 + 48);
-			    }
-			    else if (prod >= 10 && prod < 100)
-			    {
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				_putchar((prod / 10) % 10 + 48);
-				_putchar(prod % 10 + 48);
-			    }
-			    else if (prod > 99 && prod < 1000)
-			    {
-				_putchar(44);
-				_putchar(32);
-				_putchar((prod / 100) % 10 + 48);
-				_putchar((prod / 10) %  10 + 48);
-				_putchar(prod % 10 + 48);
-			    }
-		    }
-		    _putchar(10);
-	    }
-    }
+				if (col == 0)
+					_putchar('0');
+				else if (prod < 10)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+					_putchar(prod % 10 + 48);
+				}
+				else if (prod >= 10 && prod < 99)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar((prod / 10) % 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+				else if (prod > 99 && prod < 1000)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar((prod / 100) % 10 + 48);
+					_putchar((prod / 10) % 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
