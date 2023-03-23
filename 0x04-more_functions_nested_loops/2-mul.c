3baf531325abb663f05a5c78a13aee83e7be8794
gyasifred <gyasifred@gmail.com>
2023-03-23 13:04:00 +0000

    mutiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiply tw0 numbers
+ *@a: first numer
+ *@b: second number
+ *Return: multiply
+ */
+int mul(int a, int b)
+{
+	int multiply;
+
+	multiply = a * b;
+
+	return (multiply);
+}
