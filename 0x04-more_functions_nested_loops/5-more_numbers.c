1097a8fb761cfa73088f47f3b30d8245d4e4e5a6
gyasifred <gyasifred@gmail.com>
2023-03-23 15:44:52 +0000

    printing 1-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,20 @@
  */
 void more_numbers(void)
 {
-	int i, j, k;
+int i, j, k;
 
-	for (i = 0; i <= 10; i++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			k = j;
-			if (j > 9)
-			{
-				_putchar('1');
-				k = j % 10;
-			}
-			_putchar(48 + k);
-		}
-		_putchar(10);
-	}
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+k = j;
+if (j > 9)
+{
+_putchar('1');
+k = j % 10;
+}
+_putchar(48 + k);
+}
+_putchar(10);
+}
 }
