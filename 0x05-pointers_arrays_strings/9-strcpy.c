d350386bc7141549fa501cd9377b9d150625e076
gyasifred <gyasifred@gmail.com>
2023-03-31 06:04:40 +0000

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcpy - copy one string to another
+ *@dest: Destination string
+ *@src: String to be copy
+ *Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+
