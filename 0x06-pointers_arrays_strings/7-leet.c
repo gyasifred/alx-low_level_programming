e61c5857f9b047b1b37796943ea15e7475412d28
gyasifred <gyasifred@gmail.com>
2023-04-02 00:16:51 +0000

    encode strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,32 +7,19 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char s_1[] = "aAeEoOtTlL";
+	char s_2[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (j = 0; j <= 9; j++)
 		{
-			s[i] = '4';
+			if (s_1[j] == s[i])
+			{
+				s[i] = s_2[j];
+			}
 		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-		else
-			s[i] = s[i];
 	}
 	return (s);
 }
