f2f5eea2243834a8ee88c04458dfa2ac109d2220
gyasifred <gyasifred@gmail.com>
2023-04-02 00:55:50 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *rot13 - replace a character with thw 13 placecharacter atr it
+ *@s: string
+ *Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char letters[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char keys[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+	i = 0;
+	while (s[i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = keys[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
