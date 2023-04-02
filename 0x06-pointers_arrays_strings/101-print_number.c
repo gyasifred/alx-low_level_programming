8717cdacb12497775f46b35e0c9ab8eda78e2b38
gyasifred <gyasifred@gmail.com>
2023-04-02 13:25:08 +0000

    print intrgers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -31,13 +31,13 @@ void print_number(int n)
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
-	else if (n >= 100 && n < 1000)
+	else if ((n >= 100) && (n <= 999))
 	{
 		_putchar((n / 100) + '0');
 		_putchar(((n / 10) % 10) + '0');
-		_putchar(n % 10 + '0');
+		_putchar((n % 10) + '0');
 	}
-	else if (n >= 1000 && n < 10000)
+	else if ((n >= 1000) && (n <= 9999))
 	{
 		_putchar((n / 1000) + '0');
 		_putchar((n / 100) % 10 + '0');
