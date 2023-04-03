b48ddfbb2cd1f86ac432f606e4a0ae917ce4b51e
gyasifred <gyasifred@gmail.com>
2023-04-03 21:18:07 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memcpy - Copy memory area
+ *@dest: Destination string
+ *@src: Source string to copy
+ *@n: integer
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; (*(src + i) != '\0' && i < n); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
