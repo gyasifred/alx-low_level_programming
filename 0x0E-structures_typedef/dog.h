e2f7edc50ff3e63ef684a5b28ae239e169f22e8c
gyasifred <gyasifred@gmail.com>
2023-04-18 03:47:00 +0000

    typedef struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
   *@age: ge of poppy
   *@owner: ownner of poppy
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
