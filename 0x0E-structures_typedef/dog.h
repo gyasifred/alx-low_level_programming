16936d7c17203778400a669441097c4dfc4521b4
gyasifred <gyasifred@gmail.com>
2023-04-18 03:05:48 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - Home Poppy
+  *@name: name of poppy
+  *@age: ge of poppy
+  *@owner: ownner of poppy
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* ifndef DOG_H */
