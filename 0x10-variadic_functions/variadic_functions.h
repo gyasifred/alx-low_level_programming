c2d9a6219d32bdcea887564764108eb28693a1f1
gyasifred <gyasifred@gmail.com>
2023-04-21 21:44:01 +0000

    print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,17 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct op - Struct op
+ *
+ * @c: The data type (char)
+ * @f: The function associated with it
+ */
+
+typedef struct op
+{
+	char c;
+	void (*f)(va_list);
+} op_t;
 
 #endif /* #ifndef VARIADIC_FUNCTIONS_H */
